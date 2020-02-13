c88d107aa86dbc84df10585767ad9fd4bf366f3a
Dania King <dania@fortunebuilders.com>
2020-02-13 12:18:40 -0800

    finally got the page to update with the goal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,21 @@
+
+
+
+
+
+
+
+
 // Funtion to generate the Goal (a random number that has a min and max)
 function genGoal(min, max) {
   // genreate a random number between 19 and 120
   var goalNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
   // Return the random number
   return goalNum;
-  //   Update Goal number on DOM with Random Number
-  $("#goal").text("Goal: " + genGoal());
   // Log the goal number to the console
-  console.log(goalNum());
+  console.log(goalNum);
 }
+
 genGoal();
+//   Update Goal number on DOM with Random Number
+$("#goal").text("Goal: " + genGoal());
